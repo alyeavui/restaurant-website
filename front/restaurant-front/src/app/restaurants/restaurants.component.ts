9db17296c90c148565e92135667689be75db0f2c
kbAdiya <156772092+kbAdiya@users.noreply.github.com>
2025-04-11 21:15:37 +0500

    new branch adding

--- front/restaurant-front/src/app/restaurants/restaurants.component.ts
+++ front/restaurant-front/src/app/restaurants/restaurants.component.ts
@@ -5,25 +5,39 @@ import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { HeaderMainComponent } from '../components/header-main/header-main.component';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-restaurants',
-  imports: [CommonModule, RouterModule, HeaderMainComponent],
+  imports: [CommonModule, RouterModule, HeaderMainComponent, FormsModule],
   templateUrl: './restaurants.component.html',
   styleUrl: './restaurants.component.css'
 })
 export class RestaurantsComponent implements OnInit {
   restaurants: Restaurant[] = [];
-
+  filteredRestaurants: Restaurant[] = [];
+  searchTerm: string = '';
   constructor(private restaurantService: RestaurantService, private router: Router) {}
 
   ngOnInit(): void {
     this.restaurantService.getRestaurants().subscribe((data) => {
       this.restaurants = data;
       console.log(data); 
+      this.filteredRestaurants = data;
     });
   }
 
   goToRestaurantDetail(id: number): void {
     this.router.navigate(['/restaurant', id]);
+  }
+
+  
+  searchRestaurants(): void {
+    if (this.searchTerm.trim() !== '') {
+      this.filteredRestaurants = this.restaurants.filter(restaurant =>
+        restaurant.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    } else{
+      this.filteredRestaurants = this.restaurants;  
+    }
   }
 }
